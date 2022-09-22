c408c0bb6ba7da3cf4cff30d6b5cd744c1a37dd0
Tomer Ben-Rachel <tomerpacific@gmail.com>
2022-09-22 23:28:40 +0300

    TodoServer
    Removing old configuration object

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express')
 var bodyParser = require('body-parser')
 var firebase = require('firebase')
@@ -21,19 +19,6 @@ var firebaseConfig = {
   
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-const serviceAccount = {
-  "type": process.env.type,
-  "project_id": process.env.project_id,
-  "private_key_id": process.env.private_key_id,
-  "private_key": process.env.private_key.replace(/\\n/g, '\n'),
-  "client_email": process.env.client_email,
-  "client_id": process.env.client_id,
-  "auth_uri": process.env.auth_uri,
-  "token_uri": process.env.token_uri,
-  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-  "client_x509_cert_url": process.env.client_x509_cert_url
-};
 
 admin.initializeApp({
   credential: admin.credential.cert({
